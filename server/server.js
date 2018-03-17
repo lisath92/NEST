3bf50321759b55d56db9ec705deb035fd9e4c3f7
Jed Piezas <jed.piezas@gmail.com>
2018-03-17 11:19:21 -0700

    BE-2: Remove unused variable

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const app = express();
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const delimiter = "jedsincrediblylongdelimiter:";
 
 function broadcast(data) {
   wss.clients.forEach((client) => {
@@ -19,7 +18,7 @@ function broadcast(data) {
 wss.on('connection', function connection(ws, req) {
   ws.on('message', function incoming(data) {
     const {message, clientInfo: {languagePreference: language, id}} = JSON.parse(data);
-    
+
     console.log('received: %s', message);
 
     broadcast(message);
